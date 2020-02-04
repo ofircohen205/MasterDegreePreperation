77b91fc486b903862a91a4849d9e27897a3254cf
asafjo23 <asafjo23@gmail.com>
2020-02-04 10:51:41 +0200

    Longest common subsequence problem (O(n^2))

--- Playgroung.java
+++ Playgroung.java
@@ -77,8 +77,8 @@ class Playground {
             result[i][0] = lcsNode;
         }
         
-        for(int i = 1; i < a.length(); i++){
-            for(int j = 1; j < b.length(); j++){
+        for(int i = 1; i < row; i++){
+            for(int j = 1; j < columns; j++){
                 if(a.charAt(i - 1) == b.charAt(j - 1)){
                     int value = result[i - 1][j - 1].getValue() + 1;
                     LcsNode lcsNode = new LcsNode(i - 1, j - 1, value);

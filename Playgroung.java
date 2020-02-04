b233841e1ebca7d902073ba55726f996a5e4020a
asafjo23 <asafjo23@gmail.com>
2020-02-04 10:24:29 +0200

    Longest common subsequence problem (O(n^2))

--- Playgroung.java
+++ Playgroung.java
@@ -1,3 +1,23 @@
+class LcsNode{
+    
+    private double x;
+    private double y;
+    private int value;
+    
+    public LcsNode(double x, double y, int value) {
+        this.x = x;
+        this.y = y;
+        this.value = value;
+    }
+
+    public double getX() { return x; }
+    public double getY() { return y; }
+    public int getValue() { return value; }
+    public void setX(double x) { this.x = x; }
+    public void setY(double y) { this.y = y; }
+    public void setValue(int value) { this.value = value; }
+}
+
 class Playground {
     public static void main(String[ ] args) {
         int[] a = {10, 9, 20, 33, 24, 1, 30, 40, 50, 60};
@@ -29,5 +49,27 @@ class Playground {
             }
         }
         return lis;
+    }
+
+    /*
+        Longest common subsequence problem (O(n^2))
+    */
+    public static void lcs(String a, String b) {
+        
+        int row = a.length() + 1, columns = b.length() + 1;
+        
+        LcsNode[][] result = new LcsNode[row][columns];
+        
+        // Initilizer
+        for(int i = 0; i < result[0].length; i++){
+            LcsNode lcsNode = new LcsNode(0, i, 0);
+            result[0][i] = lcsNode;
+        }
+        
+        for(int i = 0; i < a.length(); i++){
+            for(int j = 0; j < b.length(); j++){
+                
+            }
+        }   
     }
 }

42c53a3c3b19bde03984a39900134b1f8c83e0a6
asafjo23 <asafjo23@gmail.com>
2020-03-18 18:22:40 +0200

    KnapsackProblem implemented

--- GreedyProblems/KnapsackProblem.java
+++ GreedyProblems/KnapsackProblem.java
@@ -1,29 +1,43 @@
 package GreedyProblems;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 /**
  * KnapsackProblem
+ * Complexity: O(nLog(n)) Why?
+ * The major part of this algorithm is sorting the items by their specific weight
  */
+class Pair<F, S> {
 
-/**
- * InnerKnapsackProblem
- */
-class Item {
+    public final F first;
+    public final S second;
 
-    private int price, weight;
+    public Pair(F first, S second) {
+        this.first = first;
+        this.second = second;
+    }
     
-    public Item(int price, int weight){
-        this.price = price;
+    @Override
+    public String toString() {
+        return "Pair{" + String.valueOf(first) + ", " + String.valueOf(second) + "}";
+    }
+}
+
+
+class Item implements Comparable {
+
+    private int profit, weight, specificWeight;
+    
+    public Item(int profit, int weight){
+        this.profit = profit;
         this.weight = weight;
+        this.specificWeight = profit / weight;
     }
 
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
+    public int getProfit() {
+        return profit;
     }
 
     public int getWeight() {
@@ -32,6 +46,32 @@ class Item {
 
     public void setWeight(int weight) {
         this.weight = weight;
+    }
+
+    public int getSpecificWeight() {
+        return specificWeight;
+    }
+
+    public void setSpecificWeight(int specificWeight) {
+        this.specificWeight = specificWeight;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        if(o instanceof Item){
+            if(this.getSpecificWeight() >= ((Item) o).getSpecificWeight()){
+                return 1;
+            }
+            else{
+                return -1;
+            }  
+        }
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Item [profit=" + profit + ", weight=" + weight + ", specificWeight=" + specificWeight + "]";
     }
 
 }
@@ -49,13 +89,44 @@ public class KnapsackProblem {
         return this.totalWeight;
     }
 
-    public void createItem(int price, int weight){
-        Item item = new Item(price, weight);
+    public void createItem(int profit, int weight){
+        Item item = new Item(profit, weight);
         items.add(item);
     }
 
     public ArrayList<Item> getItems(){
         return this.items;
+    }
+
+    public ArrayList<Pair<Item, Double>> solve(){
+        ArrayList<Pair<Item, Double>> selected = new ArrayList<Pair<Item, Double>>();
+
+        Collections.sort(this.items, new Comparator<Item>() {
+            public int compare(Item i1, Item i2) {
+               return i2.compareTo(i1);
+            }
+         });
+
+        int index = 0;
+
+        while(totalWeight > 0){
+
+            int weight = items.get(index).getWeight();
+            totalWeight = totalWeight - weight;
+
+            if(totalWeight > 0){
+                selected.add(new Pair<Item, Double>(items.get(index), Double.valueOf(1)));
+            }
+            else{ 
+                double temp = (totalWeight + weight);
+                double fracture = temp / weight;
+                selected.add(new Pair<Item, Double>(items.get(index), fracture));
+            }
+
+            index++;
+        }
+        
+        return selected;
     }
 
 }
--- GreedyProblems/Main.java
+++ GreedyProblems/Main.java
@@ -7,5 +7,16 @@ public class Main {
 
     public static void main(String[] args) {
         
+        KnapsackProblem kp = new KnapsackProblem(15);
+        kp.createItem(10, 2);
+        kp.createItem(5, 3);
+        kp.createItem(15, 5);
+        kp.createItem(7, 7);
+        kp.createItem(6, 1);
+        kp.createItem(18, 4);
+        kp.createItem(3, 1);
+        
+        System.out.println(kp.solve());
+
     }
 }

34d3a7794bbdb459a20fe99786f9dd1974ecdd3d
asafjo23 <asafjo23@gmail.com>
2020-04-06 20:29:09 +0300

    AvlTree begins

---
+++ DataStructures/AvlTree/AvlTree.java
@@ -0,0 +1,47 @@
+package AvlTree;
+
+public class AvlTree<T> {
+
+    Node<T> root;
+    
+    public boolean isBalanced(Node<T> node){
+        return Math.abs(getBalance(node)) <= 1;
+    }
+
+    public int getBalance(Node<T> node){
+        if(node == null) return 0;
+        int leftHeight = node.getLeft() == null ? 0 : node.getLeft().getHeight();
+        int rightHeight = node.getRight() == null ? 0 : node.getRight().getHeight();
+        return leftHeight - rightHeight;
+    }
+
+    public Node<T> add(Node<T> node, T value){
+        if(root == null){
+            this.root = new Node<T>(value, null, null);
+            return this.root;
+        }
+
+        return null;
+    }
+
+    private Node<T> rotateRight(Node<T> node) {
+        Node<T> a = node.getLeft();
+        Node<T> b = a.getRight();
+        a.setRight(node);
+        node.setLeft(b);
+        node.updateHeight();
+        a.updateHeight();
+        return a;
+    }
+
+    private Node<T> rotateLeft(Node<T> node) {
+        Node<T> a = node.getRight();
+        Node<T> b = a.getLeft();
+        a.setLeft(node);
+        node.setRight(b);
+        node.updateHeight();
+        a.updateHeight();
+        return a;
+    }
+
+}
---
+++ DataStructures/AvlTree/Node.java
@@ -0,0 +1,50 @@
+package AvlTree;
+
+public class Node<T> {
+    
+    private T value;
+    private Node<T> left, right;
+    private int height;
+
+    public Node(T value, Node<T> left, Node<T> right){
+        this.value = value;
+        this.left = left;
+        this.right = right;        
+        updateHeight();
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public Node<T> getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node<T> left) {
+        this.left = left;
+    }
+
+    public Node<T> getRight() {
+        return right;
+    }
+
+    public void setRight(Node<T> right) {
+        this.right = right;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void updateHeight() {
+        int leftHeight = left == null ? -1 : left.getHeight();
+        int rightHeight = right == null ? -1 : right.getHeight();
+        this.height = 1 + Math.max(leftHeight, rightHeight);
+    }
+    
+}

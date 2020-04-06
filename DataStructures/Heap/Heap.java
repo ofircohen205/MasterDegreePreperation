0b8146361ff14dd37978e1bd009f69a34ad03596
asafjo23 <asafjo23@gmail.com>
2020-04-06 18:46:41 +0300

    Create Heap.java

---
+++ DataStructures/Heap/Heap.java
@@ -0,0 +1,54 @@
+package Heap;
+
+/**
+ * Heap
+ * Number of leafs = 2^(h - 1)
+ * The root is at index 0
+ * Left son at 2K index, Right son at 2K + 1 index
+ * You can find the father of the K elemnt using  bound(k/2)
+ */
+public class Heap<T extends Comparable<T>>{
+
+    private static final int DEFAULT_CAPACITY = 10;
+    private T[] elements;
+    private int size;
+    private boolean isMinimum = true;
+
+    public Heap(){
+        this.elements = (T[]) new Comparable[DEFAULT_CAPACITY];
+        this.size = 0;
+    }
+
+    public Heap(T[] array, boolean isMinimum){
+        this.elements = array;
+        this.isMinimum = isMinimum;
+        
+        // Adding to heap
+    }
+
+    public T[] getElements() {
+        return elements;
+    }
+
+    public void setElements(T[] elements) {
+        this.elements = elements;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public boolean isMinimum() {
+        return isMinimum;
+    }
+
+    public void setMinimum(boolean isMinimum) {
+        this.isMinimum = isMinimum;
+    }
+
+    
+}

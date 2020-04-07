b109a12eba4f687c90eb1e3def67a2e2e396fde7
asafjo23 <asafjo23@gmail.com>
2020-04-07 23:47:09 +0300

    Merge sort added

--- SortingProblems/Main.java
+++ SortingProblems/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {  
         int[] arr = {6, 2, 7, 50, 23, 11, 10, 8, 9, 32, 0, 54};
-        QuickSort.quickSort(arr);
+        MergeSort.mergeSort(arr);
         for(Integer i: arr){
             System.out.print(i + " ");
         }
---
+++ SortingProblems/MergeSort.java
@@ -0,0 +1,53 @@
+public class MergeSort {
+
+    /*
+        Merge sort implementation, complexity : O(nlog(n))
+    */
+    public static void mergeSort(int[] arr){
+        mergeSort(arr, 0, arr.length - 1);
+    }
+
+    private static void mergeSort(int[] arr, int low, int high){
+        if(low < high){
+            int mid = (low + high) / 2;
+            mergeSort(arr, low, mid);
+            mergeSort(arr, mid + 1, high);
+            merge(arr, low, mid, high);
+        }
+    }
+
+    private static void merge(int[] arr, int low, int mid, int high){
+
+        int[] left = new int[mid - low + 1];
+        int[] right = new int[high - mid];
+
+        for(int i = 0; i < left.length; i++){
+            left[i] = arr[i + low];
+        }
+
+        for(int i = 0; i < right.length; i++){
+            right[i] = arr[mid + i + 1];
+        }
+
+        merge(arr, left, right, 0, 0, low);
+    }
+
+    private static void merge(int[] arr, int[] left, int[] right, int leftIndex, int rightIndex, int arrIndex){
+        if(arrIndex == arr.length){
+            return;
+        }
+        
+        if(leftIndex < left.length && rightIndex < right.length && left[leftIndex] <= right[rightIndex] || leftIndex < left.length && rightIndex >= right.length){
+            arr[arrIndex] = left[leftIndex];
+            leftIndex++;
+        }
+
+        else if(leftIndex < left.length && rightIndex < right.length && left[leftIndex] > right[rightIndex] || leftIndex >= left.length && rightIndex < right.length){
+            arr[arrIndex] = right[rightIndex];
+            rightIndex++;
+        }
+
+        merge(arr, left, right, leftIndex, rightIndex, ++arrIndex);
+    }
+
+}

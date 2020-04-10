110716cb20c4dde76ae7a4c5326366ea4d3c3f3a
asafjo23 <asafjo23@gmail.com>
2020-04-10 18:53:57 +0300

    Binary knapspack implementation starts

--- DynamicProgramming/Main.java
+++ DynamicProgramming/Main.java
@@ -1,6 +1,7 @@
 import Solutions.*;
 
 public class Main {
+
     public static void main(String[] args) {
         
         int[] dims = {5, 4, 4, 6, 6, 2, 2, 7};
--- DynamicProgramming/Solutions/BinaryKnapsackProblem.java
+++ DynamicProgramming/Solutions/BinaryKnapsackProblem.java
@@ -1,9 +1,33 @@
 package Solutions;
 
+import java.util.ArrayList;
+
 /**
  * BinaryKnapsackProblem
  */
+class Pair<F, S> {
+
+    public final F first;
+    public final S second;
+
+    public Pair(F first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+    
+    @Override
+    public String toString() {
+        return "Pair{" + String.valueOf(first) + ", " + String.valueOf(second) + "}";
+    }
+}
+
 public class BinaryKnapsackProblem {
 
+    public static ArrayList<Pair<String, Integer>> maxBinaryKnapsackProblem(int numberOfItems, int totalWeightAllowed, ArrayList<Pair<String, Integer>> items){
+
+        ArrayList<Pair<String, Integer>> reulst = new ArrayList<>();
+
+        return reulst;
+    }
     
 }

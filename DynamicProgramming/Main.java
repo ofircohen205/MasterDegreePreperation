e5df19190c5bf7db6b1fc6ed4aa48d5a6bd47e70
asafjo23 <asafjo23@gmail.com>
2020-04-11 22:48:17 +0300

    Updating merge sort

--- DynamicProgramming/Main.java
+++ DynamicProgramming/Main.java
@@ -1,4 +1,8 @@
+import java.util.Arrays;
+import java.util.List;
+
 import Solutions.*;
+import Utils.Pair;
 
 public class Main {
 
@@ -6,5 +10,7 @@ public class Main {
         
         int[] dims = {5, 4, 4, 6, 6, 2, 2, 7};
         System.out.println(MatrixMult.minMatrixMult(dims));
+        List<Pair<Integer, Integer>> items = Arrays.asList(new Pair<Integer,Integer>(1, 2), new Pair<Integer,Integer>(2, 3), new Pair<Integer,Integer>(5, 4), new Pair<Integer,Integer>(6, 5));
+        System.out.println(BinaryKnapsackProblem.maxBinaryKnapsackProblem(4, 8, items));    
     }
 }
--- DynamicProgramming/Solutions/BinaryKnapsackProblem.java
+++ DynamicProgramming/Solutions/BinaryKnapsackProblem.java
@@ -1,33 +1,65 @@
 package Solutions;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import Utils.Pair;
 
 /**
  * BinaryKnapsackProblem
+ * This is NP Hard problem, therefore, we can only produced 'reasonable' solution to this problem
+ * Assum that all weights are positive and in between 1...totalWeight for any given item;
+ * complexity: O(n*W), W can be exponantial in the value, there for this can be 2^n
  */
-class Pair<F, S> {
 
-    public final F first;
-    public final S second;
-
-    public Pair(F first, S second) {
-        this.first = first;
-        this.second = second;
-    }
-    
-    @Override
-    public String toString() {
-        return "Pair{" + String.valueOf(first) + ", " + String.valueOf(second) + "}";
-    }
-}
 
 public class BinaryKnapsackProblem {
 
-    public static ArrayList<Pair<String, Integer>> maxBinaryKnapsackProblem(int numberOfItems, int totalWeightAllowed, ArrayList<Pair<String, Integer>> items){
+    public static ArrayList<Pair<Integer, Integer>> maxBinaryKnapsackProblem(int numberOfItems, int totalWeightAllowed, ArrayList<Pair<Integer, Integer>> items){
 
-        ArrayList<Pair<String, Integer>> reulst = new ArrayList<>();
+        ArrayList<Pair<Integer, Integer>> reulst = new ArrayList<>();
 
+        // First value int the piar = profit, Second value in the pair = weight
+        Collections.sort(items, new Comparator<Pair<Integer, Integer>>() {
+            @Override
+            public int compare(final Pair<Integer, Integer> o1, final Pair<Integer, Integer> o2) {
+                if(o1.second > o2.second){
+                    return 1;
+                }
+                
+                if(o1.second < o2.second){
+                    return -1;
+                }
+                
+                return 0;
+            }
+        });
+
+        int[][] temp = new int[numberOfItems + 1][totalWeightAllowed + 1];
+
+        for(int i = 1; i <= numberOfItems; i++){
+
+            Pair<Integer, Integer> currentItem = items.get(i - 1);
+            
+            for(int j = 1; j <= totalWeightAllowed; j++){
+                if(currentItem.first <= j){
+                    temp[i][j] = Math.max(currentItem.first + temp[i][j - currentItem.second], temp[i - 1][j]);
+                }
+                else{
+                    temp[i][j] = temp[i - 1][j];
+                }
+            }
+        }
+
+        System.out.println("temp[numberOfItems][totalWeightAllowed]");
         return reulst;
     }
+
+	public static char[] maxBinaryKnapsackProblem(int numberOfItems, int totalWeightAllowed,
+			List<Pair<Integer, Integer>> items) {
+		return null;
+	}
     
 }

7d320d16827e542320622572f0c5612f83b3bdd4
asafjo23 <asafjo23@gmail.com>
2020-04-13 13:17:57 +0300

    FFT work starts

---
+++ FastFourierTransform/FFT.java
@@ -0,0 +1,17 @@
+package FastFourierTransform;
+
+/*
+ FFT to calculate DFT of polynom A
+ We want to achive the following operation in O(nlogn) time
+ Evaluation 
+ Addition
+ Multiplication
+
+ Polynom can be represented by his: coeffs, roots, samples, here we will focus on
+ coeffs and samples representation.
+ with FFT, polynom that represented with his coeffs can be converted to samples representation and vice versa in O(nlogn)
+
+*/
+public class FFT {
+
+}
---
+++ FastFourierTransform/Polynom.java
@@ -0,0 +1,27 @@
+package FastFourierTransform;
+
+/*
+ This class will represent polynom object.
+ The class will include API for: 
+ Evaluation 
+ Addition Of 2 Polynoms
+ Multiplication Of 2 Polynoms
+*/
+public class Polynom {
+
+    public Polynom(){
+
+    }
+
+    public double hornerRule(double x){
+        return 0;
+    }
+
+    public Polynom evaluation(Polynom p2){
+        return null;
+    }
+
+    public Polynom multiplication(Polynom p2){
+        return null;
+    }
+}
